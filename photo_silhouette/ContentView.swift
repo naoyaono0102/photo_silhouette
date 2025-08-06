83ad7cc7df3cddd36c05f10d104d7817158cb5c9
NAOYA ONO <naoya.ono0102@gmail.com>
2025-08-06 09:24:30 +0900

    Initial Commit

---
+++ photo_silhouette/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  photo_silhouette
+//
+//  Created by 尾野順哉 on 2025/08/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ photo_silhouette/photo_silhouetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  photo_silhouetteApp.swift
+//  photo_silhouette
+//
+//  Created by 尾野順哉 on 2025/08/06.
+//
+
+import SwiftUI
+
+@main
+struct photo_silhouetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
